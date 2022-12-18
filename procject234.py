2ef01be1483c525dfa2b503aec3f137ebd78c6ee
Veer908 <116869116+Veer908@users.noreply.github.com>
2022-12-18 19:17:26 +0530

    Add files via upload

---
+++ procject234.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+
+dataset = pd.read_csv('projectC234_EPL(1).csv')
+
+Football_club = dataset['']
